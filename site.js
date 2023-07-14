f684d5e9c954de0197f6b8ea3b171b5e114c9459
Adebukol <adebukolaomowole@gmail.com>
2023-07-14 12:27:58 +0100

    product details page setup

--- site.js
+++ site.js
@@ -13,13 +13,15 @@ function menutoggle(){
 
 const switchMode =document.getElementById("switch-btn");
 
-function darkmode(){
-  if(switchMode.src= "images/arrows_button_on.svg")
+// Function to handle toggle button click for light and dark mode
+switchMode.onclick= function () {
+  document.body.classList.toggle("dark-mode");
+  if(document.body.classList.contains("dark-mode"))
   {
     switchMode.src= "images/arrows_button_off.svg"
   }
-  else if(switchMode.src= "images/arrows_button_off.svg")
-  {
+  else {
     switchMode.src= "images/arrows_button_on.svg"
   }
 }
+
