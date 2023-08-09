7e6b9d3a77bbea20c100980ff177a2e276ee4412
Adebukol <adebukolaomowole@gmail.com>
2023-08-10 00:31:23 +0100

    new files added

--- site.js
+++ site.js
@@ -49,3 +49,51 @@ thumbnail[2].onclick = function(){
 thumbnail[3].onclick = function(){
   productImg.src = "images/image-product-4.jpg";
 }
+
+//-----------------cartjs
+const product = [
+  {
+    id:0,
+    image:"images/image-product-1.jpg",
+    title:"White and Carton Sneakers",
+    price:100,
+  },
+  {
+    id:1,
+    image:"images/black-shoe.png",
+    title:"Black Curtis shoe",
+    price:70,
+  },
+  {
+    id:2,
+    image:"images/bag4.png",
+    title:"Pink Handbag",
+    price:50,
+  },
+  {
+    id:3,
+    image:"images/blue-trainers.png",
+    title:"Blue trainers",
+    price:50,
+  }
+];
+
+const categories = [...new Set(product.map((item)=>
+  {return item}))]
+  let i=0;
+  document.getElementById("productRoot").innerHTML = categories.map((item)=>
+  {
+    var {image, title, price} = item;
+    return(
+      `<div class="row">
+        <div class="col-1">
+          <img class="img-dit" src=${image}></img>
+          </div>
+          <div class="col-1">
+          <p>${title}<p>
+          <h4>$ ${price}.00</h4>`+
+          "<button onclick ='addtocart("+(i++)+")'>Add To Cart</button>" +
+          `</div>
+          </div>`
+    )
+  }).join('')
